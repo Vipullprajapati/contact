28e4c1efb0e6c1e9d6de2f44f224535cb438b65a
Vipul Prajapati <vipulprajapati0070@gmail.com>
2025-01-03 05:06:11 +0000

    install dependence

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
+import Header from "./components/Header";
+// import uuid4, { valid } from "uuid4";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // at time of starting
+  // const [contact , setContact]=useState([]);
+
+  // local storage
+  const localStorageKey = "contact";
+
+  // at time of showing last saved data or local storage data
+  const [contact, setContact] = useState(() => {
+    return JSON.parse(localStorage.getItem(localStorageKey)) || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(contact));
+  }, [contact]);
+
+  const addContact = (data) => {
+    // console.log(data,"From App.js");
+    setContact([...contact, { id: uuid4(), data }]); // contact ki copy raho or next data ko add karo
+  };
+
+  const removeContact = (id) => {
+    const updatedList = contact.filter((val) => {
+      return val.id !== id;
+    });
+    setContact(updatedList);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+      <div className="home-page">
+        <AddContact addContact={addContact} />
+        <ContactList contact={contact} removeContact={removeContact} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
